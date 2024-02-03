d602d980efe6d432947084087153695760a4fd1d
Jean Choe <jeanchoe@cs.washington.edu>
2024-02-02 22:48:20 -0800

    Replace ListFIFOQueue.java

--- src/main/java/datastructures/worklists/ListFIFOQueue.java
+++ src/main/java/datastructures/worklists/ListFIFOQueue.java
@@ -1,6 +1,5 @@
 package datastructures.worklists;
 
-import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.FIFOWorkList;
 
 import java.util.NoSuchElementException;
@@ -11,45 +10,61 @@ import java.util.NoSuchElementException;
  */
 public class ListFIFOQueue<E> extends FIFOWorkList<E> {
 
-    public ListFIFOQueue() {
-        throw new NotYetImplementedException();
-    }
+    private Node<E> front;
+    private Node<E> back;
+    public int size;
+    public ListFIFOQueue() { this.size = 0; }
 
     @Override
     public void add(E work) {
-        throw new NotYetImplementedException();
-    }
 
+        if (front == null) {
+            front = new Node<>(work);
+            back = front;
+        } else {
+            back.next = new Node<>(work);
+            back = back.next;
+        }
+        size++;
+    }
     @Override
     public E peek() {
-        throw new NotYetImplementedException();
+        if(!hasWork()) throw new NoSuchElementException();
+        return front.data;
     }
 
     @Override
     public E next() {
-        throw new NotYetImplementedException();
+        if(!hasWork()) throw new NoSuchElementException();
+        Node<E> thing = new Node<>(front.data);
+        front = front.next;
+        size--;
+        return thing.data;
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return size;
     }
 
     @Override
     public void clear() {
-        throw new NotYetImplementedException();
+        front = null;
+        size = 0;
     }
 
-    private class ListNode {
+    public static class Node<E> {
+        public final E data;
+        public Node<E> next;
 
-        public E data;
-        public ListNode next;
-
-        public ListNode(E data) {
-            this.data = data;
-            next = null;
+        public Node(E data) {
+            this(data, null);
         }
 
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
     }
-
 }
+
