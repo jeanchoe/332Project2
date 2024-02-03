bc344c8e661e4f07bff6b0d3ee132a906696c268
Jean Choe <jeanchoe@cs.washington.edu>
2024-02-02 22:50:50 -0800

    Replace MinFourHeapComparable.java

--- src/main/java/datastructures/worklists/MinFourHeapComparable.java
+++ src/main/java/datastructures/worklists/MinFourHeapComparable.java
@@ -3,6 +3,8 @@ package datastructures.worklists;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.PriorityWorkList;
 
+import java.util.NoSuchElementException;
+
 /**
  * See cse332/interfaces/worklists/PriorityWorkList.java
  * for method specifications.
@@ -11,37 +13,83 @@ public class MinFourHeapComparable<E extends Comparable<E>> extends PriorityWork
     /* Do not change the name of this field; the tests rely on it to work correctly. */
     private E[] data;
 
+    private int size;
+
     public MinFourHeapComparable() {
-        throw new NotYetImplementedException();
+       data = (E[]) new Comparable[10];
+
+       size = 0;
     }
 
     @Override
     public boolean hasWork() {
-        throw new NotYetImplementedException();
+        return size > 0;
     }
 
     @Override
     public void add(E work) {
-        throw new NotYetImplementedException();
+        if(size == this.data.length){
+            E[] newArray = (E[]) new Comparable [this.data.length * 2];
+            for(int i = 0; i < this.data.length; i++){
+                newArray[i] = this.data[i];
+            }
+            this.data = newArray;
+        }
+        int length = size;
+
+        while (work.compareTo(this.data[(length-1) / 4]) < 0 && length > 0){
+            this.data[length] = this.data[(length - 1) / 4];
+            length = (length - 1)/4;
+
+        }
+        size ++;
+        this.data[length] = work;
     }
 
     @Override
     public E peek() {
-        throw new NotYetImplementedException();
+        if(!hasWork()){
+            throw new NoSuchElementException();
+        }
+        return data[0];
     }
 
     @Override
     public E next() {
-        throw new NotYetImplementedException();
+        if(!hasWork()){
+            throw new NoSuchElementException();
+        }
+        E next = data[0];
+        E element = data[size - 1];
+        int start = 0;
+
+        while(size - 1 > start * 4){
+            int end = 4 * start + 1;
+            for(int i = (4 * start) + 2; i < (4 * start) + 5; i++){
+                if (i < size && data[i].compareTo(data[end]) < 0) {
+                    end = i;
+                }
+            }
+            if(data[end].compareTo(element) < 0){
+                data[start] = data[end];
+                start = end;
+            } else {
+                break;
+            }
+        }
+        data[start] = element;
+        size--;
+        return next;
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return this.size;
     }
 
     @Override
     public void clear() {
-        throw new NotYetImplementedException();
+        size = 0;
+        data = (E[]) new Comparable[10];
     }
 }
